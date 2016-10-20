31e701ecbe7fffdad06b8b1d5187b5c338763ba6
shiva <g.shivachaitanya@gmail.com>
2016-10-20 11:55:51 +0530

    till table creation for each client

--- server.js
+++ server.js
@@ -1,21 +1,69 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var mysql = require("mysql");
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+  res.send(counter.toString());
+});
+
+app.get('/clientName/', function (req, res) {
+   var clientNo = req.query.name;
+   var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1",
+  database : "airok"
+  
+});
+ con.connect(function(err){
+  if(err){
+    res.send('Error connecting to Db');
+    return;
+  }
+   res.send("Connection established");
+});
+
+
+
+con.query('CREATE TABLE IF NOT EXISTS ?? (timestamp int(10) AUTO_INCREMENT, pre real(6,3),temp real(6,3),humid real(6,3),PRIMARY KEY (timestamp))', [clientNo], function (error, results) {
+  // error will be an Error if one occurred during the query
+  // results will contain the results of the query (if any)
+  if(error) throw error;
+});
+  
+ 
+});
+
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/db.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'db.js'));
+});
+
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,63 @@
-console.log('Loaded!');
+button = document.getElementById("bclick");
+
+button.onclick=function()
+{
+    var counter;var bool=0;
+   //button.disabled = true;
+   var macName = " ";
+   var request = new XMLHttpRequest();
+   request.open('GET','http://localhost:8080/counter',true);
+   request.send(null);
+   request.onreadystatechange = function()
+   {
+   	if(request.readystate = XMLHttpRequest.DONE)
+	{
+		if(request.status == 200)
+		{
+			  counter = request.responseText;
+			 
+			   var disp = "Now Happy customers are ".concat(counter.toString());
+			     var count = document.getElementById("counter");
+			   count.innerHTML = disp;
+			   
+			   
+			   //for sending client name
+			   var clientNameSending = new XMLHttpRequest();
+			   clientNameSending.onreadystatechange = function()
+			   {
+			   	if(clientNameSending.readystate = XMLHttpRequest.DONE)
+				{
+					if(clientNameSending.status == 200)
+					{
+						var ansFromServer = clientNameSending.responseText;
+						 //  alert(ansFromServer);
+		
+					}	   
+			   
+			   	}
+			   };
+	
+			    //for sending client name
+			    var check = counter.toString();
+			     macName = "client".concat(check)  ;
+			     
+			     
+			    if(check && bool==0){
+			    //alert(macName);
+			    bool =1;
+			    clientNameSending.open('GET','http://localhost:8080/clientName?name='+macName,true);
+			    clientNameSending.send(null);
+				}
+			
+					}	   
+			   
+			   	}
+			   };
+
+	   
+   
+};
+
+
+
+ 
